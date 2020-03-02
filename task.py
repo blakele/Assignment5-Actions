2b0e5f67ea184149edcbed20f6feb8e5f060c04d
blakele <49256995+blakele@users.noreply.github.com>
2020-03-01 20:04:23 -0800

    date tests

--- task.py
+++ task.py
@@ -10,3 +10,22 @@ def firstLastList(listA):
     first = listA[0]
     last = listA[len(listA)-1]
     return (first, last)
+
+
+def date(dateA, dateB):
+    a1 = dateA.find('/') +1
+    listA = dateA[a1:]
+    b1 = listA.find('/')
+    listA1 = listA[:b1]
+    numA = int(listA1)
+
+    a2 = dateB.find('/') +1
+    listB = dateB[a2:]
+    b2 = listA.find('/')
+    listB1 = listB[:b2]
+    numB = int(listB1)
+
+    temp = numA - numB
+    numC = abs(temp)
+    return numC
+
--- tests.py
+++ tests.py
@@ -19,6 +19,12 @@ class TestCase(unittest.TestCase):
     def test4(self):
         listA = [1, 2, 3, 4]
         self.assertEqual((1, 4), task.firstLastList(listA))
+    
+    def test5(self):
+        dateA = "11/12/2019"
+        dateB = "12/24/2019"
+        expected = 12
+        self.assertEqual(expected, task.date(dateA, dateB))
 
 
 if __name__ == '__main__':
